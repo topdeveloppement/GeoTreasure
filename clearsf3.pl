f8291353de59eb3cac1f8c1b63111d52aa1de75c
Dessaint <franck.dessaint@gmail.com>
2016-08-04 13:53:35 +0200

    Mise en place

---
+++ clearsf3.pl
@@ -0,0 +1,6 @@
+print("\n\nSuppression du cache ...");
+system("sudo rm -rf ./var/cache/* ./var/logs/* /var/sessions/*");
+system("sleep 0.1");
+print("\n\nAttribution des droits ...");
+system("sudo chmod 777 ./var/cache ./var/logs ./var/sessions");
+print("\n\nTerminé\n\n");
